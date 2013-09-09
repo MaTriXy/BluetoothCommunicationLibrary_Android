cc3771f2ce8485de63c13123e3956d0f53e03405
louis <louis_d@netcosports.com>
2013-09-09 11:40:35 +0200

    Add commands

---
+++ src/ktlab/lib/connection/commands/FileDataCommand.java
@@ -0,0 +1,39 @@
+package ktlab.lib.connection.commands;
+
+import android.os.Message;
+
+import java.io.InputStream;
+import java.nio.ByteOrder;
+
+import ktlab.lib.connection.CommandReceiveThread;
+import ktlab.lib.connection.ConnectionCommand;
+
+/**
+ * Created by louis on 04/09/13.
+ */
+public class FileDataCommand extends CommandReceiveThread {
+
+
+    public FileDataCommand(InputStream in, Message msg, ByteOrder order) {
+        super(in, msg, order);
+    }
+
+    @Override
+    public void run() {
+
+        //read get_user_par ACK
+        if (!readAck()) {
+            this.forceStop = true;
+            return;
+        }
+
+        final ConnectionCommand fileDataCommand = receiveFile();
+        if (fileDataCommand == null) {
+            this.forceStop = true;
+            return;
+        }
+
+        mMessage.obj = fileDataCommand;
+        mMessage.sendToTarget();
+    }
+}
